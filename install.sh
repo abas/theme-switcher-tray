2c3b3a7d0004174ab23e73df915c19867fea9325
abas <akhmadbasir5@gmail.com>
2018-05-10 08:23:53 +0700

    update config default theme

--- install.sh
+++ install.sh
@@ -17,44 +17,27 @@ USR_BIN_ROOT=$USR_ROOT/bin
 USR_SHARE_ROOT=$USR_ROOT/share
 USR_ICONS_ROOT=$USR_ROOT/icons
 
-echo ""
-echo ""
-
 echo "deploying autostart"
 # XDG ################################################
 cp $XDG/autostart/theme-switcher-tray.desktop $XDG_ROOT/autostart/
 cp $XDG/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/tealinux.xml $XDG_ROOT/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/
 
-echo ""
-echo ""
-
 echo "copying binary file"
 # BIN ################################################
 cp $USR/bin/theme-switcher-tray $USR_ROOT/bin/
-
-echo ""
-echo ""
 
 echo "making documentation"
 # SHARE ##############################################
 cp -r $USR/share/doc/theme-switcher-tray $USR_ROOT/share/doc/
 
-echo ""
-echo ""
-
 echo "installing icons"
 # ICONS ##############################################
 cp $USR/share/icons/hicolor/scalable/apps/theme-switcher-tray.svg $USR_ROOT/share/icons/hicolor/scalable/apps/
-
-echo ""
-echo ""
 
 echo "finishing install"
 # THEME-SWITCHER-TRAY ################################
 cp -r $USR/share/theme-switcher $USR_ROOT/share/
 
-echo ""
-echo ""
 echo "done~"
 
 echo "|---------------------------------|"
